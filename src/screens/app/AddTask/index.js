d1dc0d520e03ae42b2426021a808ce3d4075decf
Himanshu12145 <himansu12145@gmail.com>
2023-03-12 21:29:29 +0530

    Added Loading as well

--- src/screens/app/AddTask/index.js
+++ src/screens/app/AddTask/index.js
@@ -3,26 +3,25 @@ import {
   Image,
   Pressable,
   SafeAreaView,
-  StyleSheet,
+  ActivityIndicator,
   Text,
-  View,
+  ScrollView,
 } from 'react-native';
 import moment from 'moment';
-
+import firestore from '@react-native-firebase/firestore';
 import React, {useState} from 'react';
 import styles from './styles';
 import Title from '../../../components/Title';
 import Input from '../../../components/Input';
 import Categories from '../../../components/Categories';
 import {categories} from '../../../constants/categories';
-import DatePicker from 'react-native-date-picker';
-
 import DateInput from '../../../components/DateInput';
 import Button from '../../../components/Button';
 const AddTask = ({navigation}) => {
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState();
   const [deadline, setDeadline] = useState(new Date());
+  const [loading, setLoading] = useState(false);
   const handleBack = () => {
     navigation.goBack();
   };
@@ -33,10 +32,37 @@ const AddTask = ({navigation}) => {
       Alert.alert('Please enter the task title');
       return;
     }
+    if (!categories) {
+      Alert.alert('Please select a category');
+      return;
+    }
     if (moment(deadlineFormatted).isBefore(today)) {
       Alert.alert('Please enter future date');
       return;
     }
+
+    setLoading(true);
+    firestore()
+      .collection('Tasks')
+      .doc('ABC')
+      .set({
+        title,
+        deadline,
+        category,
+      })
+      .then(() => {
+        setLoading(false);
+        console.log('Task Added');
+        navigation.navigate('Tasks');
+        setTitle('');
+        setDeadline(new Date());
+        setCategory(null);
+      })
+      .catch(e => {
+        console.log('error adding task >>>', e);
+        setLoading(false);
+        Alert.alert(e.message);
+      });
   };
   return (
     <SafeAreaView style={styles.container}>
@@ -47,25 +73,31 @@ const AddTask = ({navigation}) => {
         />
       </Pressable>
       <Title type="thin">Add New Task</Title>
-      <Text style={styles.label}>Describe the task</Text>
-      <Input
-        value={title}
-        onChangeText={setTitle}
-        outlined
-        placeholder="Type here..."
-      />
-      <Text style={styles.label}>Type</Text>
-      <Categories
-        categories={categories}
-        selectedCategory={category}
-        onCategoryPress={setCategory}
-      />
-      <Text style={styles.label}>Deadline</Text>
-      {/* <DatePicker date={deadline} onDateChange={setDeadline} /> */}
-      <DateInput value={deadline} onChange={setDeadline} />
-      <Button type="blue" style={styles.button} onPress={onSubmit}>
-        Add the Task
-      </Button>
+      <ScrollView>
+        <Text style={styles.label}>Describe the task</Text>
+        <Input
+          value={title}
+          onChangeText={setTitle}
+          outlined
+          placeholder="Type here..."
+        />
+        <Text style={styles.label}>Type</Text>
+        <Categories
+          categories={categories}
+          selectedCategory={category}
+          onCategoryPress={setCategory}
+        />
+        <Text style={styles.label}>Deadline</Text>
+        {/* <DatePicker date={deadline} onDateChange={setDeadline} /> */}
+        <DateInput value={deadline} onChange={setDeadline} />
+        {loading ? (
+          <ActivityIndicator />
+        ) : (
+          <Button type="blue" style={styles.button} onPress={onSubmit}>
+            Add the Task
+          </Button>
+        )}
+      </ScrollView>
     </SafeAreaView>
   );
 };
